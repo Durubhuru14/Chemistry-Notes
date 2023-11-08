ee234fa8c885dc49166e36d5bd428fe6135c0128
Durubhuru <118883905+Durubhuru14@users.noreply.github.com>
2023-11-09 03:28:42 +0530

    Update cards.js
    made it changes now swiper can adjust its height according to window size(made it more mobile friendly)

--- cards.js
+++ cards.js
@@ -1,11 +1,16 @@
-// will be called whenever window size changes
-window.addEventListener('resize', function() {
-	// viewport and full window dimensions will change
-	
-	var viewport_width = window.innerWidth;
-	var viewport_height = window.innerHeight;
-    let swiper = document.querySelector(".swipper")
-    let navHeight = parseInt(document.querySelector(".navbar").clientHeight)
-    let footerHeight = parseInt(document.querySelector(".footer").clientHeight)
-    console.log(`${viewport_height}-${navHeight+footerHeight}`)
-});
+// Function to adjust the height of the content div
+function adjustContentHeight() {
+    const headerHeight = document.querySelector('header').offsetHeight;
+    const footerHeight = document.querySelector('footer').offsetHeight;
+    const windowHeight = window.innerHeight;
+    
+    const content = document.getElementById('content');
+    const newHeight = windowHeight - (headerHeight + footerHeight);
+    console.log(headerHeight)
+    content.style.height = newHeight + 'px';
+    content.style.width = (newHeight * 0.75) + 'px';
+}
+
+// Call the function on page load and window resize
+window.addEventListener('load', adjustContentHeight);
+window.addEventListener('resize', adjustContentHeight);
